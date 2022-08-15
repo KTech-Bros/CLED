33d8662a6231be4cb4862f1b76740a8b3b9c2b95
erjet <vektor0355@gmail.com>
2022-08-16 02:18:52 +0300

    VIPD-51 DB worker class

--- CLED.py
+++ CLED.py
@@ -1,6 +1,7 @@
 import zipfile,re,os,urllib.request
 import redis, psycopg2
 
+# class to work with .zip file
 class zipworker:
     def __init__(self, link):
         listoffind = re.findall("([0-9a-zA-Z_.]+.zip)",link)
@@ -12,8 +13,53 @@ class zipworker:
         with zipfile.ZipFile(self.fileName, 'r') as zipObj:
             zipObj.extractall("folder")
 
+# class to work with Redis and PostgerSQL
+class dbworker:
+    def __init__(self, pgHostAdres, pgHostPort, pgUserName, pgDataBaseName, pgDataBasePassword, pgBackUpFilePath, redisHostAdres, redisHostPort, redisDataBasepassword):
+        self.pgHostAdres = pgHostAdres
+        self.pgHostPort = pgHostPort
+        self.pgUserName = pgUserName
+        self.pgDataBaseName = pgDataBaseName
+        self.pgDataBasePassword = pgDataBasePassword
+        self.pgBackUpFilePath = pgBackUpFilePath
+        self.redisHostAdres = redisHostAdres
+        self.redisHostPort = redisHostPort
+        self.redisDataBasepassword = redisDataBasepassword
+    
+    def pingRedis(self):
 
+        r = redis.Redis(
+            host= self.redisHostAdres,
+            port= self.redisHostPort, 
+            password=  self.redisDataBasepassword)
 
+        return r.ping() 
+    
+    def pingPG(self):
+
+        try:
+            connection = psycopg2.connect(user=self.pgUserName,
+                                    password=self.pgDataBasePassword,
+                                    host=self.pgHostAdres,
+                                    port=self.pgHostPort)
+            cursor = connection.cursor()
+            postgreSQL_select_Query = "select 1"
+
+            cursor.execute(postgreSQL_select_Query)
+            mobile_records = cursor.fetchall()
+
+            one = mobile_records[0][0]
+            print(one)
+
+            if one == 1:
+                return True
+        
+        except:
+            return False
+
+    def restoreDB(self):
+        command = 'pg_restore -d postgresql://{0}:{1}@{2}:{3}/{4} -v '.format(self.pgUserName,self.pgDataBasePassword,self.pgHostAdres,self.pgHostPort,self.pgDataBaseName)
+        command = command + self.pgBackUpFilePath
 
 
 
